bfeebda3fb44490d82ef2dc8cffe55a5eb44dffc
mtk171 <mtk17.1@gmail.com>
2016-11-18 08:18:17 +0530

    new category

--- app/Category.php
+++ app/Category.php
@@ -7,11 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
-    Protected $fillable=[
-        'user_id',
-        'category_name'
-    ];
-    public function user(){
-        return $this->belongsTo('App\User');
-    }
-}
+    
